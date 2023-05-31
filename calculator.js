f59668343c08e3c1cbf836d58a140e3014ddf637
shubham suryawanshi <130492776+shubh8007@users.noreply.github.com>
2023-05-31 23:52:45 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,6 @@
+const s=require("./cal");
+console.log(s.addition(85,12))
+console.log(s.substraction(30,10))
+console.log(s.multiplication(2,3))
+console.log(s.division(40,20))
+console.log(s.square(11))
